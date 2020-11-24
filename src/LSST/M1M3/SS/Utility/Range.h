e32123883f45610c69de11549f19b0d5b1bd2887
Petr Kubánek <pkubanek@lsst.org>
2020-11-24 10:24:33 +0100

    templatize inRange functions

--- src/LSST/M1M3/SS/Utility/Range.h
+++ src/LSST/M1M3/SS/Utility/Range.h
@@ -32,31 +32,18 @@ class Range {
 public:
     static uint64_t Max(uint64_t a, uint64_t b) { return a >= b ? a : b; }
 
-    static bool InRange(float min, float max, float value) { return value >= min && value <= max; }
-    static bool InRange(double min, double max, double value) { return value >= min && value <= max; }
-    static bool InRange(int32_t min, int32_t max, int32_t value) { return value >= min && value <= max; }
+    template <typename T>
+    static bool InRange(T min, T max, T value) {
+        return value >= min && value <= max;
+    }
 
-    static int32_t CoerceIntoRange(int32_t min, int32_t max, int32_t value) {
-        return (value < min) ? min : (value > max) ? max : value;
-    }
-    static double CoerceIntoRange(double min, double max, double value) {
-        return (value < min) ? min : (value > max) ? max : value;
-    }
-    static float CoerceIntoRange(float min, float max, float value) {
+    template <typename T>
+    static T CoerceIntoRange(T min, T max, T value) {
         return (value < min) ? min : (value > max) ? max : value;
     }
 
-    static bool InRangeAndCoerce(float min, float max, float value, float* output) {
-        if (Range::InRange(min, max, value)) {
-            (*output) = value;
-            return true;
-        } else {
-            (*output) = Range::CoerceIntoRange(min, max, value);
-            return false;
-        }
-    }
-
-    static bool InRangeAndCoerce(int32_t min, int32_t max, int32_t value, int32_t* output) {
+    template <typename T>
+    static bool InRangeAndCoerce(T min, T max, T value, T* output) {
         if (Range::InRange(min, max, value)) {
             (*output) = value;
             return true;
