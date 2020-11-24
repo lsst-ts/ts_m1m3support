ea998ac744870376bce015fce91fe57328705c61
Petr Kubánek <pkubanek@lsst.org>
2020-11-24 19:12:23 +0100

    added ZIndexToActuatorId, tests for ActuatorSettings

---
+++ Tests/test_ForceActuatorApplicationSettings.cpp
@@ -0,0 +1,44 @@
+/*
+ * This file is part of LSST M1M3 SS test suite. Tests ForceController.
+ *
+ * Developed for the LSST Telescope and Site Systems.
+ * This product includes software developed by the LSST Project
+ * (https://www.lsst.org).
+ * See the COPYRIGHT file at the top-level directory of this distribution
+ * for details of code ownership.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#define CATCH_CONFIG_MAIN
+#include <catch/catch.hpp>
+
+#include <ForceActuatorApplicationSettings.h>
+
+using namespace LSST::M1M3::SS;
+
+TEST_CASE("M1M3 ForceActuator data", "[ForceActuatorApplicationSettings]") {
+    ForceActuatorApplicationSettings settings;
+
+    REQUIRE(settings.ActuatorIdToZIndex(100) < 0);
+    REQUIRE(settings.ActuatorIdToZIndex(101) == 0);
+    REQUIRE(settings.ActuatorIdToZIndex(150) < 0);
+    REQUIRE(settings.ActuatorIdToZIndex(443) == 155);
+    REQUIRE(settings.ActuatorIdToZIndex(444) < 0);
+
+    REQUIRE(settings.ZIndexToActuatorId(0) == 101);
+    REQUIRE(settings.ZIndexToActuatorId(155) == 443);
+    REQUIRE(settings.ZIndexToActuatorId(156) < 0);
+    REQUIRE(settings.ZIndexToActuatorId(-2) < 0);
+}
