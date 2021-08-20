3140dc402bf8f03f5a2819fe0c72175828cbbef3
Petr Kubánek <pkubanek@lsst.org>
2021-08-20 14:56:47 +0200

    HardpointActuatorApplicationSettings YAML (cpp part)

--- src/LSST/M1M3/SS/Settings/HardpointActuatorApplicationSettings.cpp
+++ src/LSST/M1M3/SS/Settings/HardpointActuatorApplicationSettings.cpp
@@ -30,58 +30,57 @@
 #include <boost/lexical_cast.hpp>
 #include <boost/algorithm/string.hpp>
 #include <stdio.h>
+#include <yaml-cpp/yaml.h>
+#include <spdlog/spdlog.h>
 
-namespace LSST {
-namespace M1M3 {
-namespace SS {
+using namespace LSST::M1M3::SS;
 
 void HardpointActuatorApplicationSettings::load(const std::string &filename) {
-    pugi::xml_document doc;
-    XMLDocLoad(filename, doc);
-    pugi::xpath_node node =
-            doc.select_node("//HardpointActuatorApplicationSettings/HardpointActuatorTablePath");
+    try {
+        YAML::Node doc = YAML::LoadFile(filename);
 
-    typedef boost::tokenizer<boost::escaped_list_separator<char> > tokenizer;
+        typedef boost::tokenizer<boost::escaped_list_separator<char> > tokenizer;
 
-    std::string hardpointActuatorTablePath = SettingReader::get().getFilePath(node.node().child_value());
-    std::ifstream inputStream(hardpointActuatorTablePath.c_str());
-    if (!inputStream.is_open()) {
-        throw std::runtime_error("Cannot read " + hardpointActuatorTablePath + ": " + strerror(errno));
-    }
-
-    std::string lineText;
-    int32_t lineNumber = 0;
-    this->Table.clear();
-    while (std::getline(inputStream, lineText)) {
-        boost::trim_right(lineText);
-        if (lineNumber != 0) {
-            tokenizer tok(lineText);
-            tokenizer::iterator i = tok.begin();
-            HardpointActuatorTableRow row;
-            row.Index = boost::lexical_cast<int32_t>(*i);
-            ++i;
-            row.ActuatorID = boost::lexical_cast<int32_t>(*i);
-            ++i;
-            row.XPosition = boost::lexical_cast<double>(*i);
-            ++i;
-            row.YPosition = boost::lexical_cast<double>(*i);
-            ++i;
-            row.ZPosition = boost::lexical_cast<double>(*i);
-            ++i;
-            row.Subnet = (uint8_t)boost::lexical_cast<int32_t>(*i);
-            ++i;
-            row.Address = (uint8_t)boost::lexical_cast<int32_t>(*i);
-            ++i;
-            row.SensorOffset = boost::lexical_cast<float>(*i);
-            ++i;
-            row.SensorSensitivity = boost::lexical_cast<float>(*i);
-            this->Table.push_back(row);
+        std::string hardpointActuatorTablePath =
+                SettingReader::get().getFilePath(doc["HardpointActuatorTablePath"].as<std::string>());
+        std::ifstream inputStream(hardpointActuatorTablePath.c_str());
+        if (!inputStream.is_open()) {
+            throw std::runtime_error("Cannot read " + hardpointActuatorTablePath + ": " + strerror(errno));
         }
-        lineNumber++;
+
+        std::string lineText;
+        int32_t lineNumber = 0;
+        this->Table.clear();
+        while (std::getline(inputStream, lineText)) {
+            boost::trim_right(lineText);
+            if (lineNumber != 0) {
+                tokenizer tok(lineText);
+                tokenizer::iterator i = tok.begin();
+                HardpointActuatorTableRow row;
+                row.Index = boost::lexical_cast<int32_t>(*i);
+                ++i;
+                row.ActuatorID = boost::lexical_cast<int32_t>(*i);
+                ++i;
+                row.XPosition = boost::lexical_cast<double>(*i);
+                ++i;
+                row.YPosition = boost::lexical_cast<double>(*i);
+                ++i;
+                row.ZPosition = boost::lexical_cast<double>(*i);
+                ++i;
+                row.Subnet = (uint8_t)boost::lexical_cast<int32_t>(*i);
+                ++i;
+                row.Address = (uint8_t)boost::lexical_cast<int32_t>(*i);
+                ++i;
+                row.SensorOffset = boost::lexical_cast<float>(*i);
+                ++i;
+                row.SensorSensitivity = boost::lexical_cast<float>(*i);
+                this->Table.push_back(row);
+            }
+            lineNumber++;
+        }
+        inputStream.close();
+    } catch (YAML::Exception &ex) {
+        throw std::runtime_error(fmt::format("YAML Loading {}: {}", filename, ex.what()));
     }
-    inputStream.close();
 }
 
-} /* namespace SS */
-} /* namespace M1M3 */
-} /* namespace LSST */
