016b297077cabdb8a69af088cb90503e5bfde8bb
Petr Kubánek <pkubanek@lsst.org>
2024-04-05 16:52:53 +0200

    Polished Makefiles

--- src/LSST/M1M3/SS/Controllers/BumpTestController.cpp
+++ src/LSST/M1M3/SS/Controllers/BumpTestController.cpp
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 #ifndef WITH_SAL_KAFKA
 #include <sal_MTM1M3.h>
 #endif
