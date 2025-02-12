b9f1356bc4d9e8f5d1d46e7abba81aba36e08e23
Petr Kubánek <pkubanek@lsst.org>
2025-02-12 15:24:22 +0100

    Fixed tests - new catch version

--- tests/test_ModbusBuffer.cpp
+++ tests/test_ModbusBuffer.cpp
@@ -21,7 +21,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#include <catch2/catch_test_macros.hpp>
+#include <catch2/catch.hpp>
 
 #include <cmath>
 #include <iostream>
